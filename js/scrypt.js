5de925db22c50f3d2de61366cd0e76006e53d655
Alexander <qqq777163@yandex.ru>
2023-09-19 19:54:24 +0300

    project Blanchard changed

--- js/scrypt.js
+++ js/scrypt.js
@@ -302,19 +302,26 @@ $(document).ready(function () {
 	});
 });
 
-const anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-	anchor.addEventListener("click", function (event) {
-		event.preventDefault();
-		const blockID = anchor.getAttribute('href')
-		document.querySelector('' + blockID).scrollIntoView({
-			behavior: "smooth",
-			block: "start"
-		})
-	})
+const menuLinks = document.querySelectorAll('.header__top-link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
 }
-
 
 
 
